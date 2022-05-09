0cd6a88e2028eb7c923bdc8abb4254ca34565d5a
Andrei BALTEANU <andrei.balteanu@falcontrading.ro>
2022-05-09 18:58:17 +0300

    finished footer design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,31 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 
 import "./Footer.css";
+import arrowLeft from "../../images/Footer/Icon-Left.svg";
+import arrowRight from "../../images/Footer/Icon-Right.svg";
 
 function Footer() {
   return (
-    <Flex
-      direction="row"
-      justify="space-between"
-      align="center"
-      className="footer"
-    >
-      <div>ArrowLeft</div>
-      <div>Numbers</div>
-      <div>ArrowRight</div>
+    <Flex direction="row" align="center" className="footer">
+      <Flex className="leftside-footer">
+        <Image src={arrowLeft} height="15" alt="menu" className="arrow-left" />
+      </Flex>
+      <div className="number-of-pages">
+        <div className="page-number page-selected">1</div>
+        <div className="page-number">2</div>
+        <div className="page-number">3</div>
+        <div className="page-number">4</div>
+        <div className="page-number">5</div>
+      </div>
+      <Flex justify="flex-end" className="rightside-footer">
+        <Image
+          src={arrowRight}
+          height="15"
+          alt="menu"
+          className="arrow-right"
+        />
+      </Flex>
     </Flex>
   );
 }
